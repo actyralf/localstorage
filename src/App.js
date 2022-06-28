7426e3ee1f3e6557a1dee436863cd9ca66040b7f
Ralf Siewert (DCI) <ralf.siewert-freelancer@digitalcareerinstitute.org>
2022-06-28 08:59:11 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const languages = [
+  {
+    key: "german",
+    value: "Deutsch",
+  },
+  {
+    key: "english",
+    value: "Englisch",
+  },
+  {
+    key: "spanish",
+    value: "Spanisch",
+  },
+];
 
 function App() {
+  const [language, setLanguage] = useState("english");
+  const handleChange = (event) => {
+    setLanguage(event.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Local Storage</h1>
+      <select value={language} onChange={handleChange}>
+        {languages.map((language) => {
+          return (
+            <option value={language.key} key={language.key}>
+              {language.value}
+            </option>
+          );
+        })}
+      </select>
+      <p>Selected language: {language}</p>
     </div>
   );
 }
